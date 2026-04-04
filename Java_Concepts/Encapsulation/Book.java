3b330986c9863f23f4782d7d4e93eb7a21c20701
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-04-04 08:46:43 +0200

    I made an example of encapsualtion without any help of gpt or the internet

---
+++ Java_Concepts/Encapsulation/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    private String title;
+    private int id;
+    private String author;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
---
+++ Java_Concepts/Encapsulation/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Book firstBook = new Book();
+        firstBook.setTitle("The War of Art");
+        System.out.println("The Books Title is: " + firstBook.getTitle());
+    }
+
+}
